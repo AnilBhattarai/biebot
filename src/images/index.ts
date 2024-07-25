ab0d0f1cc5713d5a3b5d1e8f47a4d9864907ed9c
Anil Bhattarai <anil.bhattarai@imark.com.np>
2024-07-25 15:27:07 +0545

    changes

---
+++ src/images/index.ts
@@ -0,0 +1,19 @@
+import hamsterExchange from "./apehead.png";
+import binanceLogo from "./binance-logo.png";
+import dollarCoin from "./dollar-coin.png";
+import dailyReward from "./daily-reward.png";
+import dailyCipher from "./daily-cipher.png";
+import dailyCombo from "./daily-combo.png";
+import mainCharacter from "./ape.png";
+import hamsterCoin from "./hamster-coin.png";
+
+export {
+  hamsterExchange,
+  binanceLogo,
+  dollarCoin,
+  dailyReward,
+  dailyCipher,
+  dailyCombo,
+  mainCharacter,
+  hamsterCoin
+};
